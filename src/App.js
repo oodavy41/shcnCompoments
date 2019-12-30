e3aa5cd2c99152cdbe24de4c90f9535f25d7806d
cocolate <c@cocolate.cc>
2019-12-30 11:13:26 +0800

    property

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from "react";
-import "./location";
-import Core from "./Core";
 export default class App extends Component {
   render() {
-    return (
-      <div style={{ width: "100%", height: "100%" }}>
-        <Core></Core>
-      </div>
-    );
+    return <div style={{ width: "100%", height: "100%" }}></div>;
   }
 }
